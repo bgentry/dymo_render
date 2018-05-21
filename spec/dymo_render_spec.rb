1dc1cacdde87aca6cea563b50c8b95db8489df9c
Blake Gentry <blakesgentry@gmail.com>
2018-05-21 13:46:09 -0700

    add Code128 sample, Roboto font

--- spec/dymo_render_spec.rb
+++ spec/dymo_render_spec.rb
@@ -3,6 +3,7 @@ require "spec_helper"
 describe DymoRender do
   let(:font_dirs) { [File.join(__dir__, "fixtures", "files")] }
   let(:sample_label_qr) { File.read(file_fixture("sample_qr.label")) }
+  let(:sample_label_qr_code128) { File.read(file_fixture("sample_qr_code128.label")) }
 
   describe ".new" do
     it "does not error" do
@@ -15,6 +16,14 @@ describe DymoRender do
 
     context "with a sample QR label" do
       let(:label_xml) { sample_label_qr }
+
+      it "does not error" do
+        expect { subject }.not_to raise_error
+      end
+    end
+
+    context "with a sample QR + Code 128 Auto label" do
+      let(:label_xml) { sample_label_qr_code128 }
 
       it "does not error" do
         expect { subject }.not_to raise_error
