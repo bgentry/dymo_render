c7c40fd7162efcf3b402f59566fda46cfa8a987f
Blake Gentry <blakesgentry@gmail.com>
2018-05-21 14:01:14 -0700

    generate PDFs on disk during tests

--- spec/dymo_render_spec.rb
+++ spec/dymo_render_spec.rb
@@ -5,6 +5,9 @@ describe DymoRender do
   let(:sample_label_qr) { File.read(file_fixture("sample_qr.label")) }
   let(:sample_label_qr_code128) { File.read(file_fixture("sample_qr_code128.label")) }
 
+  let(:sample_label_qr_rendered) { file_fixture("sample_qr.pdf") }
+  let(:sample_label_qr_code128_rendered) { file_fixture("sample_qr_code128.pdf") }
+
   describe ".new" do
     it "does not error" do
       expect { DymoRender.new(xml: sample_label_qr) }.not_to raise_error
@@ -12,13 +15,18 @@ describe DymoRender do
   end
 
   describe "#render" do
-    subject { DymoRender.new(xml: label_xml, font_dirs: font_dirs).render }
+    subject { DymoRender.new(xml: label_xml, font_dirs: font_dirs) }
 
     context "with a sample QR label" do
       let(:label_xml) { sample_label_qr }
 
       it "does not error" do
-        expect { subject }.not_to raise_error
+        expect { subject.render }.not_to raise_error
+      end
+
+      it "updates the rendered PDF on disk" do
+        pdf_result = subject.render
+        File.write(sample_label_qr_rendered, pdf_result)
       end
     end
 
@@ -26,7 +34,12 @@ describe DymoRender do
       let(:label_xml) { sample_label_qr_code128 }
 
       it "does not error" do
-        expect { subject }.not_to raise_error
+        expect { subject.render }.not_to raise_error
+      end
+
+      it "updates the rendered PDF on disk" do
+        pdf_result = subject.render
+        File.write(sample_label_qr_code128_rendered, pdf_result)
       end
     end
   end
